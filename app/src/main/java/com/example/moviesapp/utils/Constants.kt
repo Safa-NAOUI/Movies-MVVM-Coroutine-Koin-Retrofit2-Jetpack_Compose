c86015999d8678b1cc6aab3c2c26453f75503bb6
Safa-NAOUI <safa.naoui96@gmail.com>
2024-04-26 15:57:34 +0200

    add Constants

---
+++ app/src/main/java/com/example/moviesapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.moviesapp.utils
+
+class Constants {
+
+    companion object {
+        const val API_KEY = "59cd6896d8432f9c69aed9b86b9c2931"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
+        const val IMAGE_URL = "https://image.tmdb.org/t/p/w342"
+    }
+}
