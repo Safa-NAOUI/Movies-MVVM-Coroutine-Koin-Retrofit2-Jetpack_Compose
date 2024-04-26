55d9fc01b50b05079676af4301e8db23c2dde08b
Safa-NAOUI <safa.naoui96@gmail.com>
2024-04-26 19:03:29 +0200

    add Movie API service

---
+++ app/src/main/java/com/example/moviesapp/data/remote/MovieApiService.kt
@@ -0,0 +1,16 @@
+package com.example.moviesapp.data.remote
+
+import com.example.moviesapp.data.model.BaseModel
+import com.example.moviesapp.utils.Constants.Companion.API_KEY
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MovieApiService {
+
+    @GET("movie/now_playing")
+    suspend fun getPopularMovies(
+        @Query("page") page: Int,
+        @Query("with_genres") genreId: String?,
+        @Query("api_key") api_key: String = API_KEY
+    ): BaseModel
+}
