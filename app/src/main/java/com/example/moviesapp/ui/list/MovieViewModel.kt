f1f3f3a64bee699419c5447c8ef57b3610d41ad7
Safa-NAOUI <safa.naoui96@gmail.com>
2024-04-28 19:38:48 +0200

    add viewModel of movies

---
+++ app/src/main/java/com/example/moviesapp/ui/list/MovieViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.moviesapp.ui.list
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviesapp.data.model.MovieModel
+import com.example.moviesapp.data.repository.IMovieRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class MovieViewModel(private val repository: IMovieRepository) : ViewModel() {
+    private val _movies = MutableStateFlow<List<MovieModel>>(emptyList())
+    val movies: StateFlow<List<MovieModel>> = _movies
+
+    init {
+        fetchPopularMovies(page = 1, genreId = null)
+    }
+
+    private fun fetchPopularMovies(page: Int, genreId: String?) {
+        viewModelScope.launch {
+            _movies.value = repository.getPopularMovies(page, genreId)
+        }
+    }
+}
